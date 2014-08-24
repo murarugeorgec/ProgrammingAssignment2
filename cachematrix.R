0436e5769e84c268856a195452161e5586ba1b44
murarugeorgec <murarugeorgec@gmail.com>
2014-08-25 01:52:39 +0300

    solved matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+### Those implementations are 99% the same as Mr. Roger Peng (those given as
+## example). There were only needed some modifications to create the matrix
 
-## Write a short comment describing this function
+
+### This function contain some others functions like:
+####	set the matrix
+####	get the matrix
+####	set the inverse of the matrix
+				# if you want another inverse than the real one :)
+####	get the inverse
+				# first must be computed
 
 makeCacheMatrix <- function(x = matrix()) {
-#salut
+		i <- NULL
+		set <- function(y) {
+				x <<- y
+				i <<- NULL
+		}
+		get <- function() x
+		setinverse <- function(inverse) i <<- inverse
+		getinverse <- function() i
+		list(set = set, get = get,
+		 	 setinverse = setinverse,
+			 getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+### This function computes the inverse of a matrix
+### I used an error handler in case the matrix does not have an inverse
+### In this case, I preferred to print a message and assign to "i" "no inverse"
+##rather than the default value "NULL"
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+		## Return a matrix that is the inverse of 'x'
+		i <- x$getinverse()
+		if(!is.null(i)) {
+				message("getting cached data")
+				return(i)
+        }
+		data <- x$get()
+		tryCatch(
+			{i <- solve(data, ...)},
+			error = function(cond)
+				{
+				 message("The given matrix is singular")
+				 i <<- "No Inverse"
+				}
+		)
+		x$setinverse(i)
+		i
 }
